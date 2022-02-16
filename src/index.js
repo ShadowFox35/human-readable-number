9f90a5ee52e0f3e94d2231ccbfb0fd2dc6c347ef
ShadowFox35 <94312514+ShadowFox35@users.noreply.github.com>
2022-02-16 21:44:08 +0300

    add: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    number = String(number).split("");
+    let str = "";
+    if (number[0] == 0) return "zero";
+    if (number.length == 3) {
+        str += units[number[0]] + "hundred" + " ";
+        number.shift();
+    }
+    if (number.length == 2 && number[0] >= 2) {
+        str += teens[number[0]] + " ";
+        number.shift();
+    } else if (number.length == 2) {
+        var g = number[0] + number[1];
+        str += units[Number(g)];
+        return str.trim();
+    }
+    if (number[0] != 0) str += units[number[0]];
+    return str.trim();
+};
+let units = [
+    "",
+    "one ",
+    "two ",
+    "three ",
+    "four ",
+    "five ",
+    "six ",
+    "seven ",
+    "eight ",
+    "nine ",
+    "ten ",
+    "eleven ",
+    "twelve ",
+    "thirteen ",
+    "fourteen ",
+    "fifteen ",
+    "sixteen ",
+    "seventeen ",
+    "eighteen ",
+    "nineteen ",
+];
+let teens = [
+    "",
+    "",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+];
